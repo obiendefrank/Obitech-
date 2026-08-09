4cb0f56e8533285035d56b3e8d7f0da7d6f90b41
Obitech technology <198388707+obiendefrank@users.noreply.github.com>
2026-08-09 03:01:05 +0300

    Add Tawk.to chat integration script

---
+++ assets/bot.js
@@ -0,0 +1,10 @@
+var TawkAPI = TawkAPI || {}, Tawk_LoadStart = new Date();
+(function() {
+    var s1 = document.createElement("script"),
+        s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/6a77c0d6c010c21d4b63235a/1jvhsihq8';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+})();
